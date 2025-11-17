eb013a32faff0afbe5823749a2d043f3f01baac2
ClodoAldo <jotarocarioca@gmail.com>
2025-11-17 18:36:50 +0100

    Last Commit before Production
    Ultimo commit antes de começarmos a trabalhar no bot, apartir daqui inicia
    
    Co-Authored-By: xFoldM <244166056+xfoldm@users.noreply.github.com>

--- src/commands/gerais/ping.ts
+++ src/commands/gerais/ping.ts
@@ -1,11 +1,6 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-  .setName("Nome_do_Comando")
-  .setDescription("Descriçao_do_Comando")
-  .addStringOption((option) =>
-    option.setName("nome da option caso tenha").setDescription("descriçao da option").setRequired(true)
-  );
+export const data = new SlashCommandBuilder().setName("ping").setDescription("responde pong");
 
 export async function execute(interaction: ChatInputCommandInteraction) {
   await interaction.reply("Pong!");
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,62 @@
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
-import fs from "node:fs";
+import * as dotenv from "dotenv";
 import path from "node:path";
+import { loadAllCommands, LoadedCommand } from "./utils/loadCommands.js"; // Função para carregar todos os comandos do utils
+
+dotenv.config();
 
 // Extende a interface Client para adicionar a propriedade commands
 // No JavaScript puro, isso nao e necessario, mas no TypeScript sim
 declare module "discord.js" {
   interface Client {
-    commands: Collection<string, any>;
+    commands: Collection<string, LoadedCommand>;
   }
 }
 
 // Lugar aonde voce define suas variaveis de ambiente e exporta elas:
-export const token = process.env.DISCORD_TOKEN as string;
-export const clientId = process.env.CLIENT_ID as string;
-export const guildId = process.env.GUILD_ID as string;
+const token = process.env.DISCORD_TOKEN;
+const clientId = process.env.CLIENT_ID as string;
+const guildId = process.env.GUILD_ID as string;
+
 // Cria um cliente novo como todas suas instancias, e intents
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 // Quando o cliente estiver pronto, rode isso uma vez
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`To Pronto! Logado como ${readyClient.user.tag}`);
 });
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
-});
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// Evento que escuta qualquer interação
+client.on(Events.InteractionCreate, async (interaction) => {
+  // Verifica se a interação é um Chat Input Command (Slash Command)
+  if (!interaction.isChatInputCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return; // Não encontrou comando
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    if (!interaction.replied) {
+      await interaction.reply({
+        content: "❌ Ocorreu um erro ao executar este comando!",
+        ephemeral: true,
+      });
     }
   }
-}
+});
 
-// Log in to Discord with your client's token
+// Inicializa a coleçao de comandos
+client.commands = new Collection();
+
+// Carrega todos os comandos usando a função loadCommands
+const commandsPath = path.join(__dirname, "commands");
+loadAllCommands(commandsPath).then((loadedCommands) => {
+  for (const command of loadedCommands) {
+    // seta o nome do comando, e seu id
+    client.commands.set(command.data.name, command);
+  }
+});
+
+// Log in no discord
 client.login(token);
