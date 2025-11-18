1fd313099e45172c4d5a8ede314d3bb9e8a0f855
xFoldM <foldf3087@gmail.com>
2025-11-18 08:40:38 -0300

    Update data.ts
    Comentário Assets

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,7 @@ export const arrayDeDados = {
     // Objeto contendo os valores e naipes de um baralho comum
     BaralhoDeCartas: [
         {
+            // O Objeto Naipes, pode ser substituido pelos assets apenas os importando para esse Array
             Naipes: ["Ouro", "Balão", "Espada", "Trevo"],
             ValoresDasCartas: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         }
